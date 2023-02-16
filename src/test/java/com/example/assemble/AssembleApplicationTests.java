8b960952dd2601c15ab733fca31885ce38eddbe9
Eunicekk <kate4234@gmail.com>
2023-02-16 21:34:44 +0900

    assemble start

---
+++ src/test/java/com/example/assemble/AssembleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assemble;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssembleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
