24d3935233bea8e691c58ecd789fb60ded9be291
Eunicekk <kate4234@gmail.com>
2023-03-08 20:34:35 +0900

    login, findMember 메모 추가 login, findMember js 최종 수정 완료 checkMember, groupList 진행 중

--- src/main/resources/static/js/login.js
+++ src/main/resources/static/js/login.js
@@ -4,13 +4,18 @@ let loginId = document.querySelector(".login-id");
 let loginSns = document.querySelector(".login-sns");
 let icon01 = document.querySelector(".icon01");
 let icon02 = document.querySelector(".icon02");
-let loginIdInput = document.querySelector("input[type=text]");
-let loginPwInput = document.querySelector("input[type=password]");
-let loginIdDiv = document.querySelector(".login-id div:nth-child(1)");
-let loginPwDiv = document.querySelector(".login-id div:nth-child(2)");
+let loginIdInput = document.querySelector("#id-input");
+let loginPwInput = document.querySelector("#pw-input");
+let loginIdDiv = document.querySelector("#id-div");
+let loginPwDiv = document.querySelector("#password-div");
 let icon03 = document.querySelector(".icon03");
 let icon04 = document.querySelector(".icon04");
+let loginBtn = document.querySelector(".login-id button");
+let loginErrorMsg = document.querySelector(".login-error");
+let idErrorMsg = document.querySelector(".id-error");
+let pwErrorMsg = document.querySelector(".pw-error");
 
+// ID 로그인 버튼을 클릭했을 때 폼 노출시키는 이벤트
 loginIdBtn.addEventListener('click', function(){
     loginId.style.display = "flex";
     loginSns.style.display = "none";
@@ -23,6 +28,7 @@ loginIdBtn.addEventListener('click', function(){
     loginIdBtn.style.color = "#1B4568";
     loginSnsBtn.style.color = "#777777";
 })
+// SNS 로그인 버튼을 클릭했을 때 SNS 로그인 버튼들 노출시키는 이벤트
 loginSnsBtn.addEventListener('click', function(){
     loginId.style.display = "none";
     loginSns.style.display = "flex";
@@ -36,6 +42,7 @@ loginSnsBtn.addEventListener('click', function(){
     loginSnsBtn.style.color = "#1B4568";
 })
 
+// 아이디 input에 focus/blur가 갔을 때 발생하는 이벤트
 loginIdInput.addEventListener('focus', function(){
     icon03.style.color = "#1B4568";
     loginIdDiv.style.border = "1px solid #1B4568";
@@ -44,6 +51,7 @@ loginIdInput.addEventListener('blur', function(){
     icon03.style.color = "#777777";
     loginIdDiv.style.border = "1px solid #dadada";
 })
+// 비밀번호 input에 focus/blur가 갔을 때 발생하는 이벤트
 loginPwInput.addEventListener('focus', function(){
     icon04.style.color = "#1B4568";
     loginPwDiv.style.border = "1px solid #1B4568";
@@ -53,7 +61,19 @@ loginPwInput.addEventListener('blur', function(){
     loginPwDiv.style.border = "1px solid #dadada";
 })
 
-let loginErrorMsg = document.querySelector('login-error');
-let idErrorMsg = document.querySelector('id-error');
-let pwErrorMsg = document.querySelector('pw-error');
-
+// 아이디 혹은 비밀번호가 입력되지 않은 상태로 로그인 버튼을 누를 경우 경고 문구 노출
+loginBtn.addEventListener('click', function(){
+    if(loginIdInput.value === ""){
+        idErrorMsg.style.display = "flex";
+        pwErrorMsg.style.display = "none";
+        loginErrorMsg.style.display = "none";
+    }else if(loginPwInput.value === ""){
+        idErrorMsg.style.display = "none";
+        pwErrorMsg.style.display = "flex";
+        loginErrorMsg.style.display = "none";
+    }else{
+        idErrorMsg.style.display = "none";
+        pwErrorMsg.style.display = "none";
+        loginErrorMsg.style.display = "flex";
+    }
+})
